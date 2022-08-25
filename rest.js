14e4e0715623f6345fe6791a0cc778cb0e38362d
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-08-24 22:21:14 -0300

    adicionando rest

---
+++ rest.js
@@ -0,0 +1,9 @@
+function confereTamanho(...args){
+    console.log(args.length);
+}
+
+confereTamanho() ; 
+confereTamanho(1);
+confereTamanho(1,2,3,4);
+confereTamanho(1,2,3,4,5);
+confereTamanho(1,2,3,4,5,6);
